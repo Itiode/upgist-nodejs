f174fcd779fa8fa1c94ef6997705479d408a5a9c
Precious Damisa <preciousdamisa@gmail.com>
2021-10-02 23:25:08 +0100

    Update

--- src/middleware/admin.ts
+++ src/middleware/admin.ts
@@ -2,7 +2,7 @@ export default (req, res, next) => {
   const admin = req.user.roles.find((r) => r === 'Admin');
 
   if (!admin) {
-    return res.status(403).send('User is not an admin!');
+    return res.status(403).send({ message: 'User is not an admin' });
   }
 
   next();
