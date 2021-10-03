b7597ee2aae74af311791d2ed0da5dfdc9609986
Precious Damisa <preciousdamisa@gmail.com>
2021-10-03 02:44:49 +0100

    Update

--- src/controllers/news.ts
+++ src/controllers/news.ts
@@ -1,6 +1,5 @@
 import { RequestHandler } from 'express';
 
-import Article from '../models/article';
 import News from '../models/news';
 import { fetchNews } from '../services/news';
 
@@ -42,8 +41,6 @@ export const getNewsAsAdmin: RequestHandler<
               article.description === null
                 ? article.content.slice(0, 100)
                 : article.description.slice(0, 100),
-            content:
-              article.content === null ? article.description : article.content,
             category,
             url: article.url,
             urlToImage: article.urlToImage === null ? '' : article.urlToImage,
